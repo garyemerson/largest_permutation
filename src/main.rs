65dad494903c144d12004b4bcb78051070c88fe3
garyemerson <gmohammadioun@gmail.com>
2017-05-13 15:36:13 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,7 @@ fn main() {
         .split_whitespace()
         .map(|s| s.parse::<i32>().unwrap())
         .collect();
-    // max_perm(&mut nums, k);
-    max_perm2(&mut nums, n, k);
+    max_perm(&mut nums, n, k);
     let result_str = nums.iter()
         .map(|i| i.to_string())
         .collect::<Vec<String>>()
@@ -20,7 +19,7 @@ fn main() {
     println!("{}", result_str);
 }
 
-fn max_perm2(nums: &mut Vec<i32>, n: usize, max_swaps: usize) {
+fn max_perm(nums: &mut Vec<i32>, n: usize, max_swaps: usize) {
     let mut val_to_index: HashMap<i32, usize> = HashMap::new();
     for i in 0..(nums.len()) {
         val_to_index.insert(nums[i], i);
@@ -33,27 +32,9 @@ fn max_perm2(nums: &mut Vec<i32>, n: usize, max_swaps: usize) {
         if nums[i] != (desired_val) {
             let swap_index = val_to_index[&desired_val];
             swap(nums, i, swap_index);
-            *val_to_index.entry(desired_val).or_insert(0) = i;
-            *val_to_index.entry(nums[swap_index as usize]).or_insert(0) = swap_index; 
+            *val_to_index.get_mut(&desired_val).unwrap() = i;
+            *val_to_index.get_mut(&nums[swap_index]).unwrap() = swap_index; 
             num_swaps += 1;
-        }
-        i += 1;
-    }
-}
-
-fn max_perm(nums: &mut Vec<i32>, max_swaps: usize) {
-    let mut num_swaps = 0;
-    let mut i = 0usize;
-    while num_swaps < max_swaps && i < nums.len() - 1 {
-        let mut max_index = i + 1;
-        for j in i..nums.len() {
-            if nums[j] > nums[max_index] {
-                max_index = j;
-            }
-        }
-        if nums[max_index] > nums[i] {
-            num_swaps += 1;
-            swap(nums, max_index, i);
         }
         i += 1;
     }
